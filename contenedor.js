175a4d9bff1a59016138055348ecabc12277eb86
Romina Arce Aban <rarceaban@outlook.com>
2022-08-31 21:04:02 -0300

    cambios realizados

--- contenedor.js
+++ contenedor.js
@@ -3,15 +3,7 @@ const fs = require('fs')
 class Contenedor {
 
     constructor(filename) {
-        this.filename = filename,
-        this.products = []
-
-        try {
-            this.read()
-        } catch (error) {
-            console.log('No se encontro el archivo, creando uno nuevo')
-            this.write()
-        }
+        this.filename = filename
     }
 
     read() {
@@ -19,48 +11,60 @@ class Contenedor {
             .then(data => JSON.parse(data))
     }
 
-    write() {
-        return fs.promises.writeFile(this.filename, JSON.stringify(this.products))
+    write(data) {
+        const str = JSON.stringify(data, null, 2)
+        return fs.promises.writeFile(this.filename, str)
     }
 
 
 
     async save(obj) {
-        
+        try {
+            const contenido = await this.read();
+            const ultimoId = contenido[contenido.length - 1].id;
+            obj.id = ultimoId + 1;
+            const id = obj.id;
+            contenido.push(obj);
+            await this.write(contenido)
+            return id
+        } catch(error) {
+            console.log(error);
+        }
     }
     async getById(id) {
         try {
-            const products = await this.getAll()
-            const productById = products.find(p => p.id == id)
-            return productById
+            const products = await this.getAll();
+            const productById = products.find(p => p.id == id);
+            return productById;
         } catch(error) {
-            console.log(error)
-        }
-        
+            console.log(error);
+        } 
     }
     async getAll() {
         try {
             const data = await this.read();
-            return data
+            return data;
         } catch(error) {
-            console.log(error)
+            console.log(error);
         }
     }
     async deleteById(id) {
-        const idx = this.products.findIndex(p => p.id == id)
-        this.products.splice(idx, 1)
         try {
-            await this.write()
+            const data = await this.getAll();
+            const idx = data.findIndex(p => p.id == id);
+            const obj = data.splice(idx, 1);
+            await this.write(data);
+            return obj;
         } catch(error) {
-            console.log(error)
+            console.log(error);
         }
     }
     async deleteAll() {
-        this.products = []
+        const cont = [];
         try {
-            await this.write()
+            await this.write(cont);
         } catch(error) {
-            console.log(error)
+            console.log(error);
         }
     }
 }
--- test.js
+++ test.js
@@ -3,23 +3,27 @@ const Contenedor = require('./contenedor')
 async function main() {
     const product = new Contenedor('./productos.json')
 
+    console.log('Se incorporo un nuevo producto')
+    let saveProd = await product.save({ title: "regla", price: 300})
+    console.log(saveProd)
+
     console.log('Muestro todos los productos')
     let allProd = await product.getAll()
     console.log(allProd)
 
-    const idToSerch = 1
-    console.log(`Muestro el producto con id: ${idToSerch}`)
-    let productById = await product.getById(idToSerch)
+    const idBuscado = 2
+    console.log(`Muestro el producto con id: ${idBuscado}`)
+    let productById = await product.getById(idBuscado)
     console.log(productById)
 
     // console.log('Se eliminaron todos los productos')
     // let allDelete = await product.deleteAll()
     // console.log(allDelete)
 
-    // const l = 1
-    // console.log(`Elimino el producto con id: ${l}`)
-    // let deleteById = await product.deleteById(l)
-    // console.log(deleteById)
+    const deleteId = 2
+    console.log(`Elimino el producto con id: ${deleteId}`)
+    let deleteById = await product.deleteById(deleteId)
+    console.log(deleteById)
 }
 
 main()
